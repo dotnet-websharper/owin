09c04da56a0a43c2679705c9c362f05a3891f384
Loic Denuziere <loic.denuziere@intellifactory.com>
2014-11-14 16:17:07 +0100

    Fix multipart parser build

--- HttpMultipartParser/Properties/AssemblyInfo.cs
+++ HttpMultipartParser/Properties/AssemblyInfo.cs
@@ -37,7 +37,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Jake Woods 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("HttpMultipartParserUnitTest")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 

7511b2645090f08ff58ba130db0144b420a5f336
Loic Denuziere <ldenuziere@siriona.com>
2019-04-13 19:37:50 +0200

    Update build to FAKE 5

--- build.fsx
+++ build.fsx
@@ -1,15 +1,8 @@
+#r "paket: groupref build //"
 #load "paket-files/wsbuild/github.com/dotnet-websharper/build-script/WebSharper.Fake.fsx"
-open Fake
 open WebSharper.Fake
 
-let targets =
-    WSTargets.Default (fun () -> GetSemVerOf "WebSharper" |> ComputeVersion)
-    |> MakeTargets
-
-Target "Build" DoNothing
-targets.BuildDebug ==> "Build"
-
-Target "CI-Release" DoNothing
-targets.CommitPublish ==> "CI-Release"
-
-RunTargetOrDefault "Build"
+LazyVersionFrom "WebSharper"
+|> WSTargets.Default
+|> MakeTargets
+|> RunTargets
